f750e512c92cd389d78aad3562c0e52bcd8e3025
CarlosHGuzman <carlosguzmanr02@gmail.com>
2021-09-21 14:02:12 -0500

    Ejercicios del dia 21/09/2021

---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,15 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+        String nombre = "CArlos Humberto Guzman Ramirez";
+        float x = 10.25f;
+        System.out.println("x = " + x);
+        double valorDolar = 3837.25;
+        System.out.println("valorDolar = " + valorDolar);
+        float PI = (float)Math.PI;
+        System.out.println("PI = " + PI);
+        double Euler = Math.E;
+        System.out.println("Euler = " + Euler);
+    }
+}
