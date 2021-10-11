26bbe476061faae5a84176f20a40d27fa6969d17
CarlosGuzmanTIC <carlos.guzman1@o365.unab.edu.co>
2021-10-11 12:17:59 -0500

    Ejercicios del dia 11/10/2021

--- ClaseScannerEjemplos/src/PruebasScanner/Ejemplo1Scanner.java
+++ ClaseScannerEjemplos/src/PruebasScanner/Ejemplo1Scanner.java
@@ -1,21 +1,14 @@
 package PruebasScanner;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Ejemplo1Scanner {
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in); // Se crea un objeto Scanner
-        String nombre; double radio; int n;
-        System.out.print("Introduzca su nombre: "); 
-        nombre = teclado.nextLine(); // Leer un String hasta que se introduzca un enter
-        System.out.println("El nombre digitado es: " + nombre);
+
         
-        System.out.print("Introduzca el radio de la circunferencia: ");
-        radio = teclado.nextDouble(); // Leer un double
-        System.out.println("Longitud de la circunferencia: " + 2 * Math.PI * radio);
-        
-        System.out.print("Introduzca un número entero: ");
-        n = teclado.nextInt(); // Leer un entero
-        System.out.println("El cuadrado es: " + Math.pow(n, 2));
+
+        JOptionPane.showMessageDialog(null, "Hola xd\nhola  ");
+        String variable = JOptionPane.showInputDialog("Ingrese su nombre:");
     }
 }
--- EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -11,5 +11,12 @@ public class Ejercicio1 {
         System.out.println("PI = " + PI);
         double Euler = Math.E;
         System.out.println("Euler = " + Euler);
+                float a = 3, b = 5, c, d ,e, f, g;
+        c = a + b; d = a - b; e = a * b; f = a / b; g = a % b; 
+        System.out.println("La suma de las dos variables es: " + c);
+        System.out.println("La resta de las dos variables es: " + d);
+        System.out.println("La multiplicacion de las dos variables es: " + e);
+        System.out.println("La divison de las dos variables es: " + f);
+        System.out.println("El modulo de las dos variables es:  " + g);
     }
 }
